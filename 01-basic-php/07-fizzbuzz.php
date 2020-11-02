c40155aad85fca2704a3cdd1b14777dee364453b
Ben <benjamin.main88@gmail.com>
2020-11-02 14:11:27 +0000

    Question 7 - elegant

--- 01-basic-php/07-fizzbuzz.php
+++ 01-basic-php/07-fizzbuzz.php
@@ -13,3 +13,20 @@ for ($i = 1; $i <= 100; $i += 1) {
     } else dump($i);
 
 }
+
+// Elegant
+
+for ($i = 1; $i <= 100; $i += 1) {
+
+    $output = "";
+
+    if ($i % 3 === 0) {
+        $output .= "Fizz";
+    };
+
+    if ($i % 5 === 0) {
+        $output .= "Buzz";
+    }
+
+    dump($output === "" ? $i : $output);
+}
