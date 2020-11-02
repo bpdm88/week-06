8a2bc432e1d0092779f51552e39908cef2381cf1
Ben <benjamin.main88@gmail.com>
2020-11-02 17:42:44 +0000

    Question 12

---
+++ 02-basic-php/12-timesBy.php
@@ -0,0 +1,36 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+// function timesBy($numbers, $multiplier) {
+
+//     $arr = [];
+
+//     foreach ($numbers as $num) {
+//         $arr[] = $num * $multiplier;
+//     }
+
+//     return $arr;
+// }
+
+// dump(
+//     timesBy([2, 3, 4, 5, 6], 10), // [20, 30, 40, 50, 60]
+//     timesBy([2, 3, 4], 5), // [10, 15, 20]
+//     timesBy([2, 3, 4, 5, 6, 7], -5), // [-10, -15, -20, -25, -30, -35]
+//     timesBy([-3, -4, -5, -6], -5), // [15, 20, 25, 30]
+// );
+
+
+function timesBy($numbers, $multiplier) {
+
+    $arr = collect($numbers);
+
+    return $arr->map(fn($n) => $n * $multiplier);
+}
+
+dump(
+    timesBy([2, 3, 4, 5, 6], 10), // [20, 30, 40, 50, 60]
+    timesBy([2, 3, 4], 5), // [10, 15, 20]
+    timesBy([2, 3, 4, 5, 6, 7], -5), // [-10, -15, -20, -25, -30, -35]
+    timesBy([-3, -4, -5, -6], -5), // [15, 20, 25, 30]
+);
